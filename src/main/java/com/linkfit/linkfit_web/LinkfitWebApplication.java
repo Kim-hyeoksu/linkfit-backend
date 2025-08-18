2ae13d50456e40b549d23e1dd1e8cd1fa8f2dbe0
ESJung95 <eunsunjj9501@gmail.com>
2025-08-18 10:34:30 +0900

    Initial commit: Spring Boot project setup with PostgreSQL

---
+++ src/main/java/com/linkfit/linkfit_web/LinkfitWebApplication.java
@@ -0,0 +1,13 @@
+package com.linkfit.linkfit_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkfitWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkfitWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkfit/linkfit_web/LinkfitWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkfit.linkfit_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkfitWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
